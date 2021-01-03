4aaf6bd43d3546bdfd6fc0ab020275ddb23f7178
McGossy <53105381+McGossy@users.noreply.github.com>
2021-01-02 22:11:07 -0800

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Length
-
-class SearchForm(FlaskForm):
-    search = StringField('Word to Search', validators=[DataRequired(), Length(min=2, max=20)])
-
